3a0abf2391da55df528bd1cb46d237c8bd1f9285
jamesarrojo <jamesarrojo.01@gmail.com>
2024-10-21 20:15:03 +0800

    added an endpoint for a CRON job to invalidate reservations that are not paid within 10 mins

--- app/api/reservations/route.js
+++ app/api/reservations/route.js
@@ -1,5 +1,6 @@
 import { getCurrentPhilippineTime } from '@/utils/dateTimePhilippines';
 import { createClient } from '@/utils/supabase/server';
+import { DateTime } from 'luxon';
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
@@ -45,4 +46,33 @@ export async function GET(request) {
   }
   const { data, error } = await supabase.from('Reservations').select();
   return NextResponse.json({ data, error });
+}
+
+// deletes Reservations that are 10 mins or older and is not yet paid.
+export async function DELETE(request) {
+  const searchParams = request.nextUrl.searchParams;
+  const action = searchParams.get('action');
+  if (action === 'cron') {
+    const supabase = createClient();
+    const timeNow = getCurrentPhilippineTime();
+    const timeTenMinsAgo = DateTime.fromISO(timeNow).minus({ minutes: 10 });
+    const { data, error } = await supabase
+      .from('Reservations')
+      .select()
+      .eq('is_paid', false) // only include rows that are yet paid
+      .lte('created_at', timeTenMinsAgo); // checks if row is 10 or more mins old
+    // creates an array of IDs of rows to delete
+    const toDelete = data.map(({ id }) => id);
+    if (data.length > 0) {
+      const { error } = await supabase
+        .from('Reservations')
+        .delete()
+        .in('id', toDelete);
+      return NextResponse.json({ timeTenMinsAgo, data, error });
+    }
+    return NextResponse.json({
+      message: 'No reservations available to delete.',
+      error,
+    });
+  }
 }
