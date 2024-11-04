13d1c7242ea525a9f897cef8e7ba5543e4efa49a
jamesarrojo <jamesarrojo.01@gmail.com>
2024-11-05 00:48:49 +0800

    added a way to increment student's point when paying a reservation or transaction

--- app/api/reservations/[id]/route.js
+++ app/api/reservations/[id]/route.js
@@ -6,12 +6,27 @@ import { NextResponse } from 'next/server';
 export async function PUT(_, { params }) {
   const id = params.id;
 
-  const supabase = await createClient();
+  const supabase = createClient();
 
-  const { error } = await supabase
+  const { data, error } = await supabase
     .from('Reservations')
     .update({ is_paid: true })
-    .eq('id', id);
+    .eq('id', id)
+    .select()
+    .single();
+  if (!error) {
+    const startTime = DateTime.fromISO(data.start_time);
+    const endTime = DateTime.fromISO(data.end_time);
+    // Get the difference between the two times
+    const diff = endTime.diff(startTime, 'hours').hours;
+
+    // created a stored procedure using this guide https://github.com/orgs/supabase/discussions/909
+    const { error } = await supabase.rpc('increment', {
+      hours: diff,
+      row_id: data.student_id,
+    });
+    return NextResponse.json({ error });
+  }
   return NextResponse.json({ error });
 }
 export async function DELETE(_, { params }) {
