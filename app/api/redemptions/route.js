3c275ef6e5f3060b187df6132720e7d917b7fe8a
jamesarrojo <jamesarrojo.01@gmail.com>
2024-11-05 01:01:44 +0800

    added a way to decrement student's points when making a redemption_request

--- app/api/redemptions/route.js
+++ app/api/redemptions/route.js
@@ -15,6 +15,15 @@ export async function POST(request) {
     })
     .select()
     .single();
+  if (!error) {
+    // this lacks a way to not allow user to make a redemption request when his/her points are insufficient
+    // only way is on the frontend side
+    const { error } = await supabase.rpc('decrement', {
+      hours: data.total_points,
+      row_id: data.student_id,
+    });
+    return NextResponse.json({ error });
+  }
   return NextResponse.json({ data, error });
 }
 
