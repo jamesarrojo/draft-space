06c01405a15f1f0a7273f60373e330cec6405a18
jamesarrojo <jamesarrojo.01@gmail.com>
2024-11-07 21:51:10 +0800

    admin and students cannot access admin/

---
+++ app/admin/page.jsx
@@ -0,0 +1,26 @@
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+export const dynamic = 'force-dynamic';
+
+export default async function Admin() {
+  const supabase = createClient();
+  const { data: activeSession } = await supabase.auth.getSession();
+
+  if (!activeSession.session) {
+    // we use this because we can't do router.push() on a server component
+    redirect('/login');
+    // but if user is logged in and tries to access /login or /register, redirect them to dashboard
+  }
+  const userId = activeSession.session.user.id;
+  const { data: user } = await supabase
+    .from('Students')
+    .select('*')
+    .eq('supabase_id', userId)
+    .single();
+
+  // redirect to this route if account is not an admin
+  if (user) {
+    redirect('/');
+  }
+  return <div>Admin</div>;
+}
